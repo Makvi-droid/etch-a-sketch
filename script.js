2b581526d89e37bcbd618fa7a2f8d4cbc7e4183c
Makvi-droid <pasbenauro05@gmail.com>
2024-03-07 16:50:12 +0800

    added function to check if divs work

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 let sketchBoard = document.querySelector('.sketchBoard');
 sketchBoard.style.display = 'grid';
-sketchBoard.style.gridTemplateColumns = 'repeat(16, 1fr)';
-sketchBoard.style.gridTemplateRows = 'repeat(16, 1fr)';
 
+createSize(16);
+
+function createSize(size){
+    sketchBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    sketchBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let divs = size * size;
+
+    for(let i = 0; i < divs; i++){
+        let divGrid = document.createElement('div');
+        sketchBoard.insertAdjacentElement('beforeend', divGrid);
+    }
+}
+
+function getSize(){
+    let input = document.querySelector('.size').value;
+
+}
 
 
