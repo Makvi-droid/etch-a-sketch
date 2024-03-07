78e943d982080833fd0a05eb64c1a68366e55c82
Makvi-droid <pasbenauro05@gmail.com>
2024-03-07 16:11:57 +0800

    revising code

---
+++ script.js
@@ -0,0 +1,7 @@
+let sketchBoard = document.querySelector('.sketchBoard');
+sketchBoard.style.display = 'grid';
+sketchBoard.style.gridTemplateColumns = 'repeat(16, 1fr)';
+sketchBoard.style.gridTemplateRows = 'repeat(16, 1fr)';
+
+
+
